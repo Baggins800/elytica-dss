52e17f35d7d8985fcd22e30db97650bff4d321a6
Ruan Luies <omega@live.co.za>
2020-08-13 10:48:12 +0200

    fix: add Outputfile to __init__.

--- elytica_dss/__init__.py
+++ elytica_dss/__init__.py
@@ -2,6 +2,7 @@ from .service import Service
 from .user import User
 from .application import Application
 from .inputfile import InputFile
+from .outputfile import OuputFile
 from .job import Job
 from .project import Project
 

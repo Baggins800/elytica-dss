3f9ecc231976ec5799b5dd4392f5d3ff48fb36f1
Ruan Luies <omega@live.co.za>
2020-08-13 10:53:04 +0200

    fix: typo.

--- elytica_dss/__init__.py
+++ elytica_dss/__init__.py
@@ -2,7 +2,7 @@ from .service import Service
 from .user import User
 from .application import Application
 from .inputfile import InputFile
-from .outputfile import OuputFile
+from .outputfile import OutputFile
 from .job import Job
 from .project import Project
 
